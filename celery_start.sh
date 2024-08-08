cf8dc48abb06a8fc3068041112060b0362c615ad
Steve Yonkeu <yokwejuste@yahoo.com>
2024-08-08 12:53:46 +0100

    debugging start with supervisor script

--- celery_start.sh
+++ celery_start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export $(cat ~/aws-django-redis/.env | xargs)
-exec celery -A my_schedular worker --loglevel=INFO
+source /home/ubuntu/aws-django-redis/venv/bin/activate
+exec celery -A my_schedular worker --loglevel=info
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export $(cat ~/aws-django-redis/.env | xargs)
-exec gunicorn my_schedular.wsgi:application --bind 0.0.0.0:8000
+source /home/ubuntu/aws-django-redis/venv/bin/activate
+exec gunicorn --workers 3 --bind unix:/home/ubuntu/aws-django-redis/aws-django-redis.sock my_schedular.wsgi:application
